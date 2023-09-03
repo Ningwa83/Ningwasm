e6937b13f56701087c3590354d56cb596eed5d03
Ningwa83 <143104000+Ningwa83@users.noreply.github.com>
2023-09-03 18:48:20 +0800

    Update heads-tails.js

--- heads-tails.js
+++ heads-tails.js
@@ -1,8 +1,8 @@
 function coinToss(){
 
 let randomNumber = Math.random();
-if(randomNumber >= 0 && randomNumber < 0.5){document.querySelector('.js-result').innerHTML = 'The result is HEAD<img src="heads.png" class="move-icon">'}
-else if(randomNumber >= 0.5 && randomNumber < 1){document.querySelector('.js-result').innerHTML = 'The result is TAIL<img src="tails.png" class="move-icon">';
+if(randomNumber >= 0 && randomNumber < 0.5){document.querySelector('.js-result').innerHTML = 'result: HEAD<img src="heads.png" class="move-icon">'}
+else if(randomNumber >= 0.5 && randomNumber < 1){document.querySelector('.js-result').innerHTML = 'result: TAIL<img src="tails.png" class="move-icon">';
 }
 
 }
